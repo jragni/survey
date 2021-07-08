a632cf152b2bbba1e5a800f5ba3035a9f74904fa
lillianlakes <lillian.lakes@gmail.com>
2021-07-08 16:34:36 -0700

    updates the start and questions pages

--- app.py
+++ app.py
@@ -12,5 +12,72 @@ responses = []  # variable to store user responses to question
 
 @app.route('/')
 def survey_start():
-	"""Returns Landing Page"""
-	return render_template('survey_start.html')
+    """Returns Landing Page"""
+
+    return render_template('survey_start.html', 
+    title = survey.title, 
+    instructions = survey.instructions)
+
+
+@app.route('/begin', methods = ['POST'])
+def begin_survey():
+    """Redirects to the questions landing page"""
+
+    flash('Starting first question')
+    return redirect('/questions/0')   
+
+
+@app.route('/questions/<int:question_num>', methods = ['GET', 'POST'])
+def ask_questions(question_num):
+    """Handles questions and sends request to answer"""
+
+    # print(request.args)
+
+    if question_num < len(survey.questions):
+
+        breakpoint()
+        return render_template('question.html', 
+        question = survey.questions[question_num].question, 
+        choices = survey.questions[question_num].choices,
+        question_num = question_num + 1)
+        # answer = request.args['answer']
+
+        # NEXT: debug answer 
+
+    # breakpoint()
+
+    return 'Survey complete!'
+    
+
+@app.route('/answer/<int:question_num>', methods = ['GET', 'POST']) # 
+def update_answer(question_num): # question_num, answer
+    """Updates answer list and redirects to next question"""
+
+    answer = request.form['answer']
+
+    breakpoint()
+
+    responses.append(answer)
+
+    print('Should append answer', answer)
+    print(responses)
+
+    return redirect(f'{question_num}')
+    # return redirect('/questions/<int:questions_num>')
+
+
+
+
+
+# @app.route('<question_num>/<answer>') # /answer/<int:question_num>
+# def update_answer(question_num, answer):
+#     """Updates answer list and redirects to next question"""
+
+#     # session['']
+
+#     responses.append(answer)
+
+#     return redirect(f'questions/{question_num}')
+    
+    
+
